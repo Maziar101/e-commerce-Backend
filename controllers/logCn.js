b003526d8be1eafa22a47b635138a97c66c9751e
Maziar <maziarzamani85@gmail.com>
2024-05-18 00:17:29 +0330

    Create Log Controller

--- controllers/logCn.js
+++ controllers/logCn.js
@@ -1,5 +1,12 @@
+import Log from "../models/logModel.js";
+import ApiFeatures from "../utils/apiFeatures.js";
 import catchAsync from "../utils/catchAsync.js";
 
 export const getAllLog = catchAsync(async (req,res,next)=>{
-    
+    const features = new ApiFeatures(Log,req.query).filters().sort().paginate().limitFields();
+    const logs = await features.query;
+    return res.status(200).json({
+        status:"success",
+        data:{logs},
+    });
 });
