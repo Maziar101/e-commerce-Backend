cf60af1618301fd5014e361f44b50beafa512495
Maziar <maziarzamani85@gmail.com>
2024-06-02 01:34:10 +0330

    Create Product Swagger

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,152 @@
 import express from "express";
 import { createProduct, deleteProduct, getAllProducts, getProductById, updateProduct } from "../controllers/productCn.js";
 import adminOrSuperAdmin from "../middleware/adminSuperAdmin.js";
+
 const productsRoute = express.Router();
 
-productsRoute.route('/').get(getAllProducts).post(adminOrSuperAdmin,createProduct);
-productsRoute.route('/:id').get(getProductById).delete(adminOrSuperAdmin,deleteProduct).patch(adminOrSuperAdmin,updateProduct);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - name
+ *         - price
+ *         - description
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the product
+ *         name:
+ *           type: string
+ *           description: The product name
+ *         price:
+ *           type: number
+ *           description: The product price
+ *         description:
+ *           type: string
+ *           description: The product description
+ *       example:
+ *         id: d5fE_asz
+ *         name: Sample Product
+ *         price: 99.99
+ *         description: This is a sample product
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: The products managing API
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Returns the list of all the products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: The list of the products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       201:
+ *         description: The product was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get the product by id
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       200:
+ *         description: The product description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ *   patch:
+ *     summary: Update the product by the id
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: The product was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: The product was not found
+ *   delete:
+ *     summary: Remove the product by id
+ *     tags: [Products]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *     responses:
+ *       200:
+ *         description: The product was deleted
+ *       404:
+ *         description: The product was not found
+ */
+
+productsRoute.route('/').get(getAllProducts).post(adminOrSuperAdmin, createProduct);
+productsRoute.route('/:id').get(getProductById).delete(adminOrSuperAdmin, deleteProduct).patch(adminOrSuperAdmin, updateProduct);
 
 export default productsRoute;
